f5fe0df000a97ec33f6d48166695d26ba646e2bc
0lss0n97 <162359583+0lss0n97@users.noreply.github.com>
2024-03-05 19:26:31 +0100

    libban

---
+++ src/MyJDBC.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MyJDBC {
+    public static void main(String[] args) {
+
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-test", "root", "Sxvbcetg97!");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from bok");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("Titel"));
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
